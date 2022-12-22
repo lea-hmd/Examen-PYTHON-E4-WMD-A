8638c90c092f55704202dc66e1939bcd7a3b0ec4
Léa Hamadouche <leahamadouche@hotmail.com>
2022-12-22 11:26:27 +0100

    feat(classes): :wastebasket: Removing the uppercase filename

--- Airport.py
+++
@@ -1,6 +0,0 @@
-class Airport:
-    def __init__(self, name: str, code: str, lat: float, long: float):
-        self.name = name # Nom de l'aéroport
-        self.code = code  # Code de l'aéroport
-        self.lat = lat  # Latitude de l'aéroport
-        self.long = long  # Longitude de l'aéroport
--- Flight.py
+++
@@ -1,5 +0,0 @@
-class Flight:
-    def __init__(self, src_code: str, dst_code: str, duration: float):
-        self.src_code = src_code # Code de l'aéroport source
-        self.dst_code = dst_code  # Code de l'aéroport de destination
-        self.duration = duration  # Durée du vol
