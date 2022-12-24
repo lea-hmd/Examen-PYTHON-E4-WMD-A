8d93561685b18e44b1f680a44246c6e60040aa95
Léa Hamadouche <leahamadouche@hotmail.com>
2022-12-24 19:26:06 +0100

    feat(flight path): :construction: Refacto FlightPathBroken and FlightPathDuplicate exceptions

--- flight_path.py
+++ flight_path.py
@@ -1,50 +1,12 @@
-from airport import Airport
-from flight import Flight
-
-
 class FlightPathBroken(Exception):
     """
-    FlightPathBroken is a class which herits from Exception and represent an error where two consecutive flight doesn't have common airports.
-
-    Args:
-        Exception (_type_): Exception
+    An exception to be raised when two consecutive flights do not have a common airport.
     """
     pass
 
 
 class FlightPathDuplicate(Exception):
     """
-    FlightPathDuplicate is a class which herits from Exception and represent an error where the path contains duplicate airport.
-
-    Args:
-        Exception (_type_): Exception
+    An exception to be raised when a flight path contains two flights to the same airport.
     """
     pass
-
-
-class FlightPath:
-    def __init__(self, src_airport: Airport) -> None:
-     self.path = [src_airport]
-     self.flights_list = []
-
-    def add(self, dst_airport: Airport, via_flight: Flight) -> None:
-        if self.path[-1].code != via_flight.src_code:
-            raise FlightPathBroken(
-                "Le vol ajouté ne passe pas par le dernier aéroport du chemin.")
-        self.path.append(dst_airport)
-        self.flights_list.append(via_flight)
-
-    def flights(self) -> list[Flight]:
-        return self.flights_list
-
-    def airports(self) -> list[Airport]:
-        return self.path
-
-    def steps(self) -> float:
-        return len(self.path) - 1
-
-    def duration(self) -> float:
-        total_duration = 0
-        for flight in self.flights_list:
-            total_duration += flight.duration
-        return total_duration
