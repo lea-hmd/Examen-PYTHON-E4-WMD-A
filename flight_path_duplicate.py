ab7e1244da77eb71eaca81ef3d10befb5bc812f8
Léa Hamadouche <leahamadouche@hotmail.com>
2022-12-24 19:48:55 +0100

    clean: :wastebasket: Remove unused files

--- flight_path_duplicate.py
+++
@@ -1,8 +0,0 @@
-class FlightPathDuplicate(Exception):
-    """
-    FlightPathDuplicate is a class which herits from Exception and represent an error where the path contains duplicate airport.
-
-    Args:
-        Exception (_type_): Exception
-    """
-    pass
--- main.py
+++
@@ -1,7 +0,0 @@
-from flight_map import FlightMap as FM
-fm = FM()
-fm.import_airports("aeroports.csv")
-fm.import_flights("flights.csv")
-
-fm.airports()
-fm.flights()
